3f3ea1702c3c60cecec81a98e464d1f2896be1ef
prerna <prerna.ip.21@gmail.com>
2020-09-13 14:52:21 +0530

    Implemented Interfaces and classes for Domain core

---
+++ MicroRabbit/MicroRabbit.Domain.Core/Bus/IEventHandler.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using MicroRabbit.Domain.Core.Events;
+
+namespace MicroRabbit.Domain.Core.Bus
+{
+   public interface IEventHandler<in TEvents>: IEventHandler
+        where TEvents:Event
+    {
+
+        Task Handle(TEvents @event);
+    }
+
+    public interface IEventHandler
+    {
+    }
+}
